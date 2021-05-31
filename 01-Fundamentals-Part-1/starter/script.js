d07be41d05eb5d593c94327e6b0dcfc9dfc35080
kobe jacobs <kobe.jcbs@gmail.com>
2021-05-31 23:36:40 +0200

    Finished lecture 12 Data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,11 @@
+// Linking a js file
+
 let js = 'amazing';
 // Tell console to show output in console which are multiple values to form a outcome
 console.log(40+8+23-10);
+
+
+// Values and variables
 
 // Value 
 console.log("Ebbe");
@@ -11,3 +16,19 @@ let firstName = "Kobe";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+
+// Data types
+
+// Print value of variable
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// Show data type of value or variable
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+
+// Change value of variable
+javascriptIsFun = "KOBE IS SEXY";
+// Print out data type of changed variable
+console.log(typeof javascriptIsFun);
