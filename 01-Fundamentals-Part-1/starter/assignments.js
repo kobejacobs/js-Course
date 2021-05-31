8dd3a457c03bc56e75a57045abb3878fb7003819
kobe jacobs <kobe.jcbs@gmail.com>
2021-05-31 23:49:09 +0200

    Finished assignment data types

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,20 @@
-// 1. Declare variables called 'country', 'continent' and 'population' and
-// assign their values according to your own country (population in millions)
+// // 1. Declare variables called 'country', 'continent' and 'population' and
+// // assign their values according to your own country (population in millions)
+// let country = "Sweden";
+// let continent = "Europe";
+// let population = "10.2 million";
+// // 2. Log their values to the console
+// console.log(country,continent,population);
+
+
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
 let country = "Sweden";
 let continent = "Europe";
 let population = "10.2 million";
-// 2. Log their values to the console
-console.log(country,continent,population);
+let isIsland = true;
+let language;
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+console.log(isIsland,country,continent,population);
