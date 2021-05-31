8a36d3ff233db31255b8e23169301a548c376dfa
kobe jacobs <kobe.jcbs@gmail.com>
2021-05-31 23:06:24 +0200

    Lecture Values and Variables done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,13 @@
 let js = 'amazing';
-      if(js === 'amazing') alert("JavaScript is FUN!");
+// Tell console to show output in console which are multiple values to form a outcome
+console.log(40+8+23-10);
 
-      // Tell console to show output in console
-      console.log(40+8+23-10);
+// Value 
+console.log("Ebbe");
+// Declaring variable and give it a value
+let firstName = "Kobe";
+
+// Ouput value of variable
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
