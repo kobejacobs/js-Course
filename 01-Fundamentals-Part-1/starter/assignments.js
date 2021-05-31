db5b9911203dc7ca7f6edfad50e19317ee74f84a
kobe jacobs <kobe.jcbs@gmail.com>
2021-06-01 00:18:31 +0200

    Finished assignment Basic Operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -21,15 +21,41 @@
 // console.log(isIsland,country,continent,population);
 
 // Let, Const, Var
-// 1. Set the value of 'language' to the language spoken where you live (some
-// countries have multiple languages, but just choose one)
+// // 1. Set the value of 'language' to the language spoken where you live (some
+// // countries have multiple languages, but just choose one)
+// let country = "Sweden";
+// let continent = "Europe";
+// let population = "10.2 million";
+// let isIsland = true;
+// // let language = "Swedish";
+// // 2. Think about which variables should be const variables (which values will never
+// // change, and which might change?). Then, change these variables to const.
+// const language = "Swedish";
+// // 3. Try to change one of the changed variables now, and observe what happens
+// console.log(isIsland,country,continent,population,language);
+
+
+// Basic Operators
+// 1. If your country split in half, and each half would contain half the population,
+// then how many people would live in each half?
 let country = "Sweden";
 let continent = "Europe";
-let population = "10.2 million";
+let population = 10200000;
 let isIsland = true;
-// let language = "Swedish";
-// 2. Think about which variables should be const variables (which values will never
-// change, and which might change?). Then, change these variables to const.
 const language = "Swedish";
-// 3. Try to change one of the changed variables now, and observe what happens
-console.log(isIsland,country,continent,population,language);
+
+let halfPopulation = population / 2;
+console.log(halfPopulation);
+// 2. Increase the population of your country by 1 and log the result to the console
+population++;
+console.log(population);
+// 3. Finland has a population of 6 million. Does your country have more people than
+// Finland?
+console.log(population > 6000000);
+// 4. The average population of a country is 33 million people. Does your country
+// have less people than the average country?
+console.log(population > 33000000);
+// 5. Based on the variables you created, create a new variable 'description'
+// which contains a string with this format: 'Portugal is in Europe, and its 11 million
+// people speak portuguese'
+let description = "Portugal is in Europe, and its 11 million people speak portuguese";
