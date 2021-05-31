6171f11f8802a60183b574f135eb50bc6c16ff70
kobe jacobs <kobe.jcbs@gmail.com>
2021-05-31 23:11:19 +0200

    Linked second script to index.html and finished assignment

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+let country = "Sweden";
+let continent = "Europe";
+let population = "10.2 million";
+// 2. Log their values to the console
+console.log(country,continent,population);
