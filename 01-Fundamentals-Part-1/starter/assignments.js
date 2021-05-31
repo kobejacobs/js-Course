4f568d1e2775af1dd789dd156f4aec5428a4c73a
kobe jacobs <kobe.jcbs@gmail.com>
2021-05-31 23:54:25 +0200

    Finished assignment let,const,var

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,3 +1,4 @@
+// Values and Variables
 // // 1. Declare variables called 'country', 'continent' and 'population' and
 // // assign their values according to your own country (population in millions)
 // let country = "Sweden";
@@ -6,15 +7,29 @@
 // // 2. Log their values to the console
 // console.log(country,continent,population);
 
+// Date Types
+// // 1. Declare a variable called 'isIsland' and set its value according to your
+// // country. The variable should hold a Boolean value. Also declare a variable
+// // 'language', but don't assign it any value yet
+// let country = "Sweden";
+// let continent = "Europe";
+// let population = "10.2 million";
+// let isIsland = true;
+// let language;
+// // 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// // to the console
+// console.log(isIsland,country,continent,population);
 
-// 1. Declare a variable called 'isIsland' and set its value according to your
-// country. The variable should hold a Boolean value. Also declare a variable
-// 'language', but don't assign it any value yet
+// Let, Const, Var
+// 1. Set the value of 'language' to the language spoken where you live (some
+// countries have multiple languages, but just choose one)
 let country = "Sweden";
 let continent = "Europe";
 let population = "10.2 million";
 let isIsland = true;
-let language;
-// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
-// to the console
-console.log(isIsland,country,continent,population);
+// let language = "Swedish";
+// 2. Think about which variables should be const variables (which values will never
+// change, and which might change?). Then, change these variables to const.
+const language = "Swedish";
+// 3. Try to change one of the changed variables now, and observe what happens
+console.log(isIsland,country,continent,population,language);
