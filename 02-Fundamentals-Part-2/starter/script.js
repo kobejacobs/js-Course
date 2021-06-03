1a0d5e5c4faf1a29d2d3e760e2163a8450b79737
kobe jacobs <kobe.jcbs@gmail.com>
2021-06-03 23:42:48 +0200

    Finished Arrow Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -36,7 +36,7 @@ console.log(applejuice);
 const appleOrangeJuice = fruitProcessor(2,4);
 console.log(appleOrangeJuice);
 
-*/
+
 
 // Function Declaration vs Expressions
 // Function Declaration
@@ -49,7 +49,30 @@ const age1 = calcAge(1991);
 const calcAge2 = function (birthYear){
     return 2037 - birthYear;
 }
-
 const age2 = calcAge2(1991);
+
 console.log(age1,age2);
 
+*/
+
+// Arrow Functions
+// Function Expression
+const calcAge2 = function (birthYear){
+    return 2037 - birthYear;
+}
+
+// Arrow function
+// Faster and easier to write and the return happens explicitly after the 'arrow'
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+
+console.log(age3);
+
+// Return manually when using more code and variables
+const yearsUntilRetirment = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntilRetirment(2001, "Kobe"));
