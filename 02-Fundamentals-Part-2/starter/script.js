973cba33c95b30f2d8ee245b3eacef621c21e56f
kobe jacobs <kobe.jcbs@gmail.com>
2021-06-03 23:20:15 +0200

    Finished Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -2,6 +2,7 @@
 // Forbids us to do certain things, shows us errors in the console
 'use strict';
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -11,5 +12,26 @@ if (passTest){
 if (hasDriversLicense){
     console.log("I can drive!");
 }
+// Tells us about certain reserved words to avoid errors
+// const interface = "audio";
+*/
 
-const interface = "audio";
+// Functions
+function logger(){
+    console.log("My name is Kobe");
+}
+// Invoking/Calling function
+logger();
+// Using parameters
+function fruitProcessor(apples, oranges){
+    console.log(apples,oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+// Call function with parameters and store the returned value in a variable
+const applejuice = fruitProcessor(5, 0);
+// print output
+console.log(applejuice);
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
