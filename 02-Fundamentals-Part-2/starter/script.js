df3b32bad62aed0656d024ddb6e3c4a058b7a400
kobe jacobs <kobe.jcbs@gmail.com>
2021-06-04 00:12:17 +0200

    Finished Coding Challenge 1

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -77,7 +77,7 @@ const yearsUntilRetirment = (birthYear, firstName) => {
 }
 console.log(yearsUntilRetirment(2001, "Kobe"));
 
-*/
+
 
 // Functions calling other functions
 function cuttingMachine(fruit){
@@ -94,3 +94,34 @@ function fruitProcessor(apples, oranges){
 }
 
 console.log(fruitProcessor(2,3));
+
+*/
+
+// Coding Challenge 1
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+// A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+// Your tasks:
+// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+const averageScore = (score1,score2,score3) => {
+    return (score1 + score2 + score3) / 3;
+}
+// 2. Use the function to calculate the average for both teams
+const averageDolphins = averageScore(44,23,71);
+const averageKoalas = averageScore(65,54,49);
+// 3. Create a function 'checkWinner' that takes the average score of each team
+// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)"
+function checkWinner(avgDolpinhs,avgKoalas){
+    if(avgDolpinhs > avgKoalas * 2){
+        return `Dolphins win (${avgDolpinhs} vs. ${avgKoalas})`;
+    } else if (avgDolpinhs * 2 < avgKoalas){
+        return `Koalas win (${avgDolpinhs} vs. ${avgKoalas})`;
+    } else{
+        return `no one wins`;
+    }
+}
+// 4. Use the 'checkWinner' function to determine the winner for both Data1 and Data 2
+console.log(checkWinner(averageDolphins,averageKoalas));
+// 5. Ignore draws this time
+// Test data:
+// § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49 § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
