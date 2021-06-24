51d694f0a6c8f93a28ca6ab441d47b0c9b9000ce
kobe jacobs <kobe.jcbs@gmail.com>
2021-06-24 22:56:07 +0200

    idk

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -375,7 +375,7 @@ for(let i=0; i <= kobe.length - 1; i++){
     console.log(kobe[i]);
 }
 
-*/
+
 
 
 // Coding Challenge 4
@@ -411,3 +411,5 @@ console.log(tips, totals);
 // 4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
 // 4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
 // 4.3. Call the function with the 'totals' array
+
+*/
